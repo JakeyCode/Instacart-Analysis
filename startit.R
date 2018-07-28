11217514ebb90fe4755289f992bdfacc2f58cc4b
JakeyCode <41838537+JakeyCode@users.noreply.github.com>
2018-07-28 14:44:00 -0400

    Hello world

---
+++ startit.R
@@ -0,0 +1,38 @@
+
+# # import the data -------------------------------------------------------
+
+# # way 1
+# DataLoc <- "C:/Users/JakeSSD/Documents/Instacart Data/instacart_2017_05_01"
+# 
+# FileList <- list.files(DataLoc,pattern=".+\\.csv",full.names = TRUE)
+# 
+# InstaData <- lapply(FileList, read.csv)
+
+# way 2
+DataLoc <- "C:/Users/JakeSSD/Documents/Instacart Data/instacart_2017_05_01"
+
+FileList <- list.files(DataLoc,pattern=".+\\.csv")
+
+NameList <-gsub('^(.+).csv',"\\1",FileList)
+
+if(exists("temp")){
+  rm(temp)
+}
+for (i in NameList) {
+  if(!exists("temp")){
+    temp <- as.numeric("1")
+  }
+  # get(i) <- read.csv(paste0(DataLoc, "/", FileList[temp]))
+  assign(i,read.csv(paste0(DataLoc, "/",FileList[temp])))
+    temp <- temp+1
+  
+}
+
+str(ls())
+
+str(get(ls()[1:length(ls())]))
+
+# Whats this for?
+# assign(dataName,read.csv(FileList))
+# Is this needed?
+# InstaData <- lapply(FileList, read.csv)
